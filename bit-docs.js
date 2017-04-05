66534be8dc8d2834b78879c4800c9f0cf3ca2dc9
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-05 10:48:38 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,3 +1,13 @@
+/**
+ * @module {function} bit-docs-dev
+ * @parent plugins
+ *
+ * @description General development tags.
+ *
+ * @body
+ *
+ * TBD
+ */
 var tags = require("./tags/tags");
 module.exports = function(bitDocs){
     bitDocs.register("tags", tags);
