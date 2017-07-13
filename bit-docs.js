e1d2258ad9ee180ac1b71ca23f8fe1e1c488861a
Joel Kuzmarski <leoj3n@gmail.com>
2017-07-13 17:44:28 -0500

    Update bit-docs.js to list tags

--- bit-docs.js
+++ bit-docs.js
@@ -7,7 +7,15 @@
  *
  * @body
  *
- * TBD
+ * This plugin registers onto the `tags` hook to add tags useful to developers
+ * for writing documentation:
+ *   - [bit-docs-dev/tags/author @author]
+ *   - [bit-docs-dev/tags/deprecated @deprecated]
+ *   - [bit-docs-dev/tags/group @group]
+ *   - [bit-docs-dev/tags/iframe @iframe]
+ *   - [bit-docs-dev/tags/inherits @inherits]
+ *   - [bit-docs-dev/tags/page @page]
+ *   - [bit-docs-dev/tags/release @release]
  */
 var tags = require("./tags/tags");
 module.exports = function(bitDocs){
